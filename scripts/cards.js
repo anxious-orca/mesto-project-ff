75af269f4d1aee792e1b9260c2e59258e0cb3ab5
anxious-orca <whatupmyglipglops@yandex.ru>
2025-01-26 16:18:22 +0300

    cards added

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "ЖК Пресня Сити",
+      link: "https://images.unsplash.com/photo-1639488903799-ad83f99150bb?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Останкинская телебашня",
+      link: "https://images.unsplash.com/photo-1628578170146-238a7ee38396?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Храм Христа Спасителя",
+      link: "https://images.unsplash.com/photo-1635415476578-ca2fae23040d?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Лахта-центр",
+      link: "https://images.unsplash.com/photo-1691856618885-d99a0ff70ced?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Собор Непорочного зачатия Пресвятой Девы Марии",
+      link: "https://images.unsplash.com/photo-1643262292778-95ca4f4adb0e?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Москва-Сити",
+      link: "https://images.unsplash.com/photo-1641286828448-9d6958a455ab?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+const container = document.querySelector('.content');
 
-// @todo: DOM узлы
+const cardsContainer = container.querySelector('.places__list');
+const editProfileButton = container.querySelector('.profile__edit-button');
+const addCardButton = container.querySelector('.profile__add-button');
 
-// @todo: Функция создания карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = cardTemplate.querySelector('.places__item');
 
-// @todo: Функция удаления карточки
+function addCard(cardTitle, cardImageSrc, functionDelete) {
+    const cardElement = cardList.cloneNode(true);
 
-// @todo: Вывести карточки на страницу
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+    cardElement.querySelector('.card__image').src = cardImageSrc;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', functionDelete);
+
+    cardElement.querySelector('.card__like-button').addEventListener('click', evt => {
+        const likeButton = evt.target;
+        likeButton.classList.toggle('card__like-button_is-active');
+    });
+
+    cardsContainer.append(cardElement);
+}
+
+function deleteCard(evt) {
+    const deleteButton = evt.target;
+    deleteButton.parentElement.remove();
+}
+
+initialCards.forEach(item => {
+    addCard(item.name, item.link, deleteCard);
+});
